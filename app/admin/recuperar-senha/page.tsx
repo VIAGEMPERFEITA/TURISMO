840fa75c56475fe23da0c28e0fca4228a8e8e185
Matheus Oliviera <matheusoliviera@MacBook-Pro-de-Matheus.local>
2026-08-07 14:43:08 -0300

    Corrige recuperação de senha do CRM

---
+++ app/admin/recuperar-senha/page.tsx
@@ -0,0 +1,5 @@
+import { AdminPasswordRecovery } from "../../../components/admin-password-recovery";
+
+export default function Page() {
+  return <main className="admin-login"><AdminPasswordRecovery/></main>;
+}
